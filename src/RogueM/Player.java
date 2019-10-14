1133b6439c38ae89693167b4387f48ad70ec0413
martinB <martin.bjersten@gmail.com>
2019-10-14 12:59:56 +0200

    added constructor

--- src/RogueM/Player.java
+++ src/RogueM/Player.java
@@ -1,7 +1,14 @@
 package RogueM;
 
 public class Player {
-    private String name = "Martin";
+    private String name;
+    private int health;
+
+    public Player(String name, int health){
+        this.name = name;
+        this.health = health;
+    }
+
 
 
     public String getName() {
