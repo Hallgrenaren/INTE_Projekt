2f90033300881f9aab3196404a2c895098ec1fce
Martin Hallgren <martin@tada.se>
2019-10-14 13:24:34 +0200

    asfdasfa

--- src/RogueM/Player.java
+++ src/RogueM/Player.java
@@ -2,15 +2,12 @@ package RogueM;
 
 public class Player {
     private String name;
-    private int health;
-    private int strength;
-    private int no;
 
-    public Player(String name, int health){
-        this.name = name;
-        this.health = health;
+    public String getName() {
+        return name;
     }
 
-
-
+    public void setName(String name) {
+        this.name = name;
+    }
 }
