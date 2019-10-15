5be52292df5f3448b84bcb9dcc84900f6d57bf21
martinB <martin.bjersten@gmail.com>
2019-10-15 13:54:18 +0200

    Added Hashmaps to Map class

--- src/RogueM/Map.java
+++ src/RogueM/Map.java
@@ -1,12 +1,35 @@
 package RogueM;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Map {
 
-    private HashMap<Position, Set<Monster>> monsterInPosition = new HashMap<>();
+    private HashMap<Position, Set<Monster>> monstersInPosition = new HashMap<>();
     private HashMap<Position, Set<Item>> itemsInPosition = new HashMap<>();
+
+    public void addItem(Position position, Item item){
+        Set<Item> items = itemsInPosition.get(position);
+        if (itemsInPosition == null) {
+            items = new HashSet<Item>();
+            itemsInPosition.put(position, items);
+        }
+        items.add(item);
+        itemsInPosition.put(position,items);
+
+    }
+
+    public void addMonster(Position position, Monster monster){
+        Set<Monster> monsters = monstersInPosition.get(position);
+        if (monstersInPosition == null) {
+            monsters = new HashSet<Monster>();
+            monstersInPosition.put(position, monsters);
+        }
+        monsters.add(monster);
+        monstersInPosition.put(position,monsters);
+
+    }
 
 
 }
