7f69072987d91a97a4dd5f704cb871619bc4ee30
martinB <martin.bjersten@gmail.com>
2019-10-15 11:55:28 +0200

    added map and position class + constructor values

--- src/RogueM/Character.java
+++ src/RogueM/Character.java
@@ -2,16 +2,16 @@ package RogueM;
 
 abstract class Character {
     private String name;
-    private int health;
-    private int speed;
-    private int damage;
+    private int health = 100;
+    private int speed = 10;
+    private int damage =10;
     private ArrayList<Item> inventory = new ArrayList<Item>();
 
     public Character(String name, int health, int speed, int damage) {
         this.name = name;
-        this.health = health ;
-        this.speed = speed;
-        this.damage = damage;
+        this.health += health;
+        this.speed += speed;
+        this.damage += damage;
     }
 
     public int getHealth() {
