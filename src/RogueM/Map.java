b7f890d452cbef519dd2b4f07ef606f999e747b0
martinB <martin.bjersten@gmail.com>
2019-10-15 13:27:44 +0200

    Changes made to Map and Position classes

--- src/RogueM/Map.java
+++ src/RogueM/Map.java
@@ -1,4 +1,12 @@
 package RogueM;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Map {
+
+    private HashMap<Position, Set<Monster>> monsterInPosition = new HashMap<>();
+    private HashMap<Position, Set<Item>> itemsInPosition = new HashMap<>();
+
+
 }
--- src/RogueM/Position.java
+++ src/RogueM/Position.java
@@ -1,11 +1,21 @@
 package RogueM;
 
+import java.util.ArrayList;
+
 public class Position {
     private int x;
     private int y;
 
 
     public Position(int x, int y) {
+        if(x<1)
+            x = 1;
+        else if(x>10)
+            x = 10;
+        if(y<1)
+            y = 1;
+        else if(y>10)
+            y = 10;
         this.x = x;
         this.y = y;
 
