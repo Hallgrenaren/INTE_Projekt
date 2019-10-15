0cf51add39597310b853850ffdba87a85b2a9553
martinB <martin.bjersten@gmail.com>
2019-10-15 11:36:48 +0200

    added constructor

---
+++ src/RogueM/Character.java
@@ -0,0 +1,29 @@
+package RogueM;
+
+public class Character {
+    private String name;
+    private int health;
+    private int speed;
+    private int damage;
+    private ArrayList<Item> inventory = new ArrayList<Item>();
+
+
+
+    public Character(String name) {
+        this.health = 100;
+        this.speed = 10;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- test/RogueM/PlayerTest.java
+++ test/RogueM/PlayerTest.java
@@ -1,7 +1,22 @@
 package RogueM;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+
+    @Test
+    void testConstructorValidValues() {
+
+        Character p = new Character("Steve");
+
+        assertEquals("Steve", p.getName());
+        assertEquals(100, p.getHealth());
+        assertEquals(10, p.getSpeed());
+
+
+    }
+
 
 }
