28602f3182af1e21f45cec6d90d9ccbb64412a4c
Martin Hallgren <martin@tada.se>
2019-10-21 14:50:25 +0200

    Checking if .gitignore does its job, cleaned up some code

--- test/RogueM/PlayerTest.java
+++ test/RogueM/PlayerTest.java
@@ -2,7 +2,6 @@ package RogueM;
 
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
 import java.awt.event.KeyEvent;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,7 +40,7 @@ class PlayerTest {
     @Test
     public void testPClassInValidValues() {
         Player p = new Player("Zhasam", "arg");
-        assertEquals("Warrior", p.getpClass());
+        assertEquals("Warrior", p.getPClass());
 
     }
 
